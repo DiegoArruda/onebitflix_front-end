b745d2a3b099b7be3206d469d11d8fe5086107c1
Diego Arruda <arrudadiego1@gmail.com>
2023-09-17 02:27:08 -0300

    Criação do header da página homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import HeaderNoAuth from "@/src/components/homeNoAuth/HeaderNoAuth";
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
 
@@ -5,9 +6,17 @@ const HomeNotAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."
+        />
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth />
+      </main>
     </>
   );
 };
---
+++ src/components/homeNoAuth/HeaderNoAuth/index.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "./styles.module.scss";
+import { Container, Button } from "reactstrap";
+import Link from "next/link";
+
+const HeaderNoAuth = () => {
+  return (
+    <>
+      <div className={styles.ctaSection}>
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+        <p>Se cadastre para ter acesso aos cursos</p>
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+      </div>
+      <Container className={styles.nav}>
+        <img
+          src="/logoOnebitflix.svg"
+          alt="logoOnebitflix"
+          className={styles.imgLogoNav}
+        />
+        <div>
+          <Link href="/login">
+            <Button className={styles.navBtn} outline>
+              Entrar
+            </Button>
+          </Link>
+          <Link href="/register">
+            <Button className={styles.navBtn} outline>
+              Quero fazer parte
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default HeaderNoAuth;
