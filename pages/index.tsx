288c75049e3211468fee62629f63d1067035cf14
Diego Arruda <arrudadiego1@gmail.com>
2023-09-19 11:42:44 -0300

    Animação da homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,10 @@ import SlideSection from "@/src/components/homeNoAuth/slideSection";
 import courseService, { CourseType } from "@/src/services/courseService";
 import { GetStaticProps } from "next";
 import Head from "next/head";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import styles from "../styles/homeNoAuth.module.scss";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 interface IndexPageProps {
   children?: ReactNode;
@@ -15,6 +17,10 @@ interface IndexPageProps {
 }
 
 const HomeNotAuth = ({ course }: IndexPageProps) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <>
       <Head>
@@ -27,12 +33,21 @@ const HomeNotAuth = ({ course }: IndexPageProps) => {
         />
       </Head>
       <main>
-        <div className={styles.sectionBackground}>
+        <div
+          className={styles.sectionBackground}
+          data-aos="fade-zoom-in"
+          data-aos-duration="1600"
+        >
           <HeaderNoAuth />
           <PresentationSection />
         </div>
-        <CardsSection />
-        <SlideSection newestCourses={course} />
+        <div data-aos="fade-right" data-aos-duration="1200">
+          <CardsSection />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350">
+          <SlideSection newestCourses={course} />
+        </div>
+
         <Footer />
       </main>
     </>
