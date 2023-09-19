390614d7bafb07e0ae2d2910f97f11c4965bd3b0
Diego Arruda <arrudadiego1@gmail.com>
2023-09-19 11:11:46 -0300

    Finalização dos slides

--- src/components/homeNoAuth/slideSection/index.tsx
+++ src/components/homeNoAuth/slideSection/index.tsx
@@ -11,11 +11,11 @@ interface props {
 const SlideSection = ({ newestCourses }: props) => {
   return (
     <>
-      <Container>
+      <Container fluid className="d-flex flex-column align-items-center py-5">
         <p className={styles.sectionTitle}>AULAS JÁ DISPONÍVEIS</p>
         <SlideComponent course={newestCourses} />
         <Link href="/register">
-          <Button outline color="light" className={styles.slideSelectionBtn}>
+          <Button outline color="light" className={styles.slideSectionBtn}>
             Se cadastre para acessar!
           </Button>
         </Link>
