f42fbb7fd437ca8dd7ddeec8d0e599ef21f0fa32
Diego Arruda <arrudadiego1@gmail.com>
2023-09-20 20:09:59 -0300

    Criação dos elements da seção de lançamentos

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,5 @@
 import FeaturedSection from "@/src/components/homeAuth/featuredSection";
+import NewestCategory from "@/src/components/homeAuth/newestCategory";
 import Head from "next/head";
 
 const HomeAuth = () => {
@@ -10,6 +11,7 @@ const HomeAuth = () => {
       </Head>
       <main>
         <FeaturedSection />
+        <NewestCategory />
       </main>
     </>
   );
