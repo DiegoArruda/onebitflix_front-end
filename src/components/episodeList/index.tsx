cbe10238a149d4638168b486b8c99c2734d1a24b
Diego Arruda <arrudadiego1@gmail.com>
2023-09-25 03:13:07 -0300

    Página do episódio completa

--- src/components/episodeList/index.tsx
+++ src/components/episodeList/index.tsx
@@ -1,11 +1,15 @@
-import { EpisodeType } from "@/src/services/courseService";
+import { CourseType, EpisodeType } from "@/src/services/courseService";
+import { useRouter } from "next/router";
 import styles from "./styles.module.scss";
 
 interface props {
   episode: EpisodeType;
+  course: CourseType;
 }
 
-const EpisodeList = ({ episode }: props) => {
+const EpisodeList = ({ episode, course }: props) => {
+  const router = useRouter();
+
   const handleSecondsToMin = (totalSeconds: number) => {
     const minutes = Math.floor(totalSeconds / 60);
 
@@ -19,9 +23,14 @@ const EpisodeList = ({ episode }: props) => {
 
     return result;
   };
+
+  const handleEpisodePlayer = () => {
+    router.push(`/course/episode/${episode.order - 1}?courseid=${course.id}`);
+  };
+
   return (
     <>
-      <div className={styles.episodeCard}>
+      <div className={styles.episodeCard} onClick={handleEpisodePlayer}>
         <div className={styles.episodeOrderTime}>
           <p className={styles.episodeOrder}>Episódio Nº {episode.order}</p>
           <p className={styles.episodeTime}>
